955fb693d13485da4edae167efd15bffc138d7a3
赖骏劼 <laijj@asiainfo.com>
2024-06-15 23:04:25 +0800

    @bpascal@防止闪退处理
    @bpascal@防止闪退处理

--- app/src/main/java/com/example/usb/ptp/PtpUsbService.java
+++ app/src/main/java/com/example/usb/ptp/PtpUsbService.java
@@ -14,6 +14,7 @@ import android.hardware.usb.UsbManager;
 import android.media.ExifInterface;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.Looper;
 import android.provider.SyncStateContract;
 import android.util.Log;
 import android.widget.Toast;
@@ -33,288 +34,299 @@ import java.util.Map;
 import java.util.Set;
 
 public class PtpUsbService implements PtpService,
-        Camera.CameraListener,
-        Camera.StorageInfoListener {
-    private final String TAG = "PtpUsbService";
-    private static final String ACTION_USB_PERMISSION = "com.jsbn.jbox.USB_PERMISSION";
+		Camera.CameraListener,
+		Camera.StorageInfoListener {
+	private final String TAG = "PtpUsbService";
+	private static final String ACTION_USB_PERMISSION = "com.jsbn.jbox.USB_PERMISSION";
 
-    private final UsbManager usbManager;
-    private final Context mContext;
-    private PtpCamera mCamera;
-    private PtpListener mListener;
+	private final UsbManager usbManager;
+	private final Context mContext;
+	private PtpCamera mCamera;
+	private PtpListener mListener;
 
-    private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            synchronized (this) {
-                String action = intent.getAction();
-                if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
-                    connect();
-                } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
-                    // 关闭相机设备
-                    stopCamera();
-                } else if (ACTION_USB_PERMISSION.equals(action)) {
-                    connect();
-                }
-            }
-        }
-    };
+	/**
+	 * loop是否启动标识
+	 */
+	private boolean looperStarted = false;
 
-    public PtpUsbService(Context context) {
-        this.usbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
-        this.mContext = context;
-    }
+	private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			synchronized (this) {
+				String action = intent.getAction();
+				if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
+					connect();
+				} else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+					// 关闭相机设备
+					stopCamera();
+				} else if (ACTION_USB_PERMISSION.equals(action)) {
+					connect();
+				}
+			}
+		}
+	};
 
-    @Override
-    public void start() {
-        // 注册监听
-        registerUsbReceiver();
-        connect();
-    }
+	public PtpUsbService(Context context) {
+		this.usbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
+		this.mContext = context;
+	}
 
-    @Override
-    public void stop() {
-        // 取消事件监听
-        unregisterUsbReceiver();
-        stopCamera();
-    }
+	@Override
+	public void start() {
+		// 注册监听
+		registerUsbReceiver();
+		connect();
+	}
 
-    private void stopCamera() {
-        // 关闭相机
-        if (mCamera != null) {
-            mCamera.shutdown();
-        }
-        if (Globe.storageIdBeans != null) {
-            if (Globe.storageIdBeans.size() > 0) {
-                Globe.storageIdBeans.clear();
-            }
-        }
-    }
+	@Override
+	public void stop() {
+		// 取消事件监听
+		unregisterUsbReceiver();
+		stopCamera();
+	}
 
-    private void registerUsbReceiver() {
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
-        filter.addAction(ACTION_USB_PERMISSION);
-        filter.setPriority(1000);
-        this.mContext.registerReceiver(usbReceiver, filter);
-    }
+	private void stopCamera() {
+		// 关闭相机
+		if (mCamera != null) {
+			mCamera.shutdown();
+		}
+		if (Globe.storageIdBeans != null) {
+			if (Globe.storageIdBeans.size() > 0) {
+				Globe.storageIdBeans.clear();
+			}
+		}
+	}
 
-    private void unregisterUsbReceiver() {
-        this.mContext.unregisterReceiver(usbReceiver);
-    }
+	private void registerUsbReceiver() {
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+		filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+		filter.addAction(ACTION_USB_PERMISSION);
+		filter.setPriority(1000);
+		this.mContext.registerReceiver(usbReceiver, filter);
+	}
 
-    private boolean connect() {
-        // 关闭相机设备
-        stopCamera();
-        UsbDevice device = null;
-        Map<String, UsbDevice> deviceList = usbManager.getDeviceList();
-        for (Map.Entry<String, UsbDevice> e : deviceList.entrySet()) {
-            UsbDevice d = e.getValue();
-            if (d.getVendorId() == PtpConstants.CanonVendorId
-                    || d.getVendorId() == PtpConstants.NikonVendorId
-                    || d.getVendorId() == PtpConstants.SonyVendorId) {
-                device = d;
-                break;
-            }
-        }
-        if (device == null) {
-            this.mListener.onNoCameraFound();
-            return false;
-        }
+	private void unregisterUsbReceiver() {
+		this.mContext.unregisterReceiver(usbReceiver);
+	}
 
-        // 判断是否有权限，没有则请求授权
-        if (!usbManager.hasPermission(device)) {
-            PendingIntent mPermissionIntent = PendingIntent.getBroadcast(
-                    this.mContext,
-                    0,
-                    new Intent(ACTION_USB_PERMISSION),
-                    0);
-            usbManager.requestPermission(device, mPermissionIntent);
-            this.mListener.onError("相机未授权");
-            return false;
-        }
+	private boolean connect() {
+		// 关闭相机设备
+		stopCamera();
+		UsbDevice device = null;
+		Map<String, UsbDevice> deviceList = usbManager.getDeviceList();
+		for (Map.Entry<String, UsbDevice> e : deviceList.entrySet()) {
+			UsbDevice d = e.getValue();
+			if (d.getVendorId() == PtpConstants.CanonVendorId
+					|| d.getVendorId() == PtpConstants.NikonVendorId
+					|| d.getVendorId() == PtpConstants.SonyVendorId) {
+				device = d;
+				break;
+			}
+		}
+		if (device == null) {
+			this.mListener.onNoCameraFound();
+			return false;
+		}
 
-        for (int i = 0, n = device.getInterfaceCount(); i < n; ++i) {
-            UsbInterface anInterface = device.getInterface(i);
-            if (anInterface.getEndpointCount() != 3) {
-                continue;
-            }
+		// 判断是否有权限，没有则请求授权
+		if (!usbManager.hasPermission(device)) {
+			PendingIntent mPermissionIntent = PendingIntent.getBroadcast(
+					this.mContext,
+					0,
+					new Intent(ACTION_USB_PERMISSION),
+					0);
+			usbManager.requestPermission(device, mPermissionIntent);
+			this.mListener.onError("相机未授权");
+			return false;
+		}
 
-            UsbEndpoint in = null;
-            UsbEndpoint out = null;
-            for (int e = 0, en = anInterface.getEndpointCount(); e < en; ++e) {
-                UsbEndpoint endpoint = anInterface.getEndpoint(e);
-                if (endpoint.getType() == UsbConstants.USB_ENDPOINT_XFER_BULK) {
-                    if (endpoint.getDirection() == UsbConstants.USB_DIR_IN) {
-                        in = endpoint;
-                    } else if (endpoint.getDirection() == UsbConstants.USB_DIR_OUT) {
-                        out = endpoint;
-                    }
-                }
-            }
-            if (in == null || out == null) {
-                continue;
-            }
+		for (int i = 0, n = device.getInterfaceCount(); i < n; ++i) {
+			UsbInterface anInterface = device.getInterface(i);
+			if (anInterface.getEndpointCount() != 3) {
+				continue;
+			}
 
-            if (device.getVendorId() == PtpConstants.CanonVendorId) {
-                PtpUsbConnection connection = new PtpUsbConnection(
-                        usbManager.openDevice(device),
-                        in,
-                        out,
-                        device.getVendorId(),
-                        device.getProductId(),
-                        device
-                );
-                mCamera = new EosCamera(connection, this);
-            } else if (device.getVendorId() == PtpConstants.NikonVendorId) {
-                PtpUsbConnection connection = new PtpUsbConnection(
-                        usbManager.openDevice(device),
-                        in,
-                        out,
-                        device.getVendorId(),
-                        device.getProductId(),
-                        device
-                );
-                mCamera = new NikonCamera(connection, this);
-            } else if (device.getVendorId() == PtpConstants.SonyVendorId) {
-                // 索尼只是做测试，未投入使用
-                Log.i(TAG, "获取到sony设备" + device.getVendorId());
-                PtpUsbConnection connection = new PtpUsbConnection(
-                        usbManager.openDevice(device),
-                        in,
-                        out,
-                        device.getVendorId(),
-                        device.getProductId(),
-                        device
-                );
-                mCamera = new SonyCamera(connection, this);
-            } else {
-                //其他设备
-                PtpUsbConnection connection = new PtpUsbConnection(
-                        usbManager.openDevice(device),
-                        in,
-                        out,
-                        device.getVendorId(),
-                        device.getProductId(),
-                        device
-                );
-                mCamera = new PtpCamera(connection, this) {
-                    @Override
-                    protected void onOperationCodesReceived(Set<Integer> operations) {
-                    }
+			UsbEndpoint in = null;
+			UsbEndpoint out = null;
+			for (int e = 0, en = anInterface.getEndpointCount(); e < en; ++e) {
+				UsbEndpoint endpoint = anInterface.getEndpoint(e);
+				if (endpoint.getType() == UsbConstants.USB_ENDPOINT_XFER_BULK) {
+					if (endpoint.getDirection() == UsbConstants.USB_DIR_IN) {
+						in = endpoint;
+					} else if (endpoint.getDirection() == UsbConstants.USB_DIR_OUT) {
+						out = endpoint;
+					}
+				}
+			}
+			if (in == null || out == null) {
+				continue;
+			}
 
-                    @Override
-                    protected void queueEventCheck() {
-                    }
-                };
-                Log.e(TAG, "不支持的设备id" + device.getVendorId());
-            }
-            return true;
-        }
+			if (device.getVendorId() == PtpConstants.CanonVendorId) {
+				PtpUsbConnection connection = new PtpUsbConnection(
+						usbManager.openDevice(device),
+						in,
+						out,
+						device.getVendorId(),
+						device.getProductId(),
+						device
+				);
+				mCamera = new EosCamera(connection, this);
+			} else if (device.getVendorId() == PtpConstants.NikonVendorId) {
+				PtpUsbConnection connection = new PtpUsbConnection(
+						usbManager.openDevice(device),
+						in,
+						out,
+						device.getVendorId(),
+						device.getProductId(),
+						device
+				);
+				mCamera = new NikonCamera(connection, this);
+			} else if (device.getVendorId() == PtpConstants.SonyVendorId) {
+				// 索尼只是做测试，未投入使用
+				Log.i(TAG, "获取到sony设备" + device.getVendorId());
+				PtpUsbConnection connection = new PtpUsbConnection(
+						usbManager.openDevice(device),
+						in,
+						out,
+						device.getVendorId(),
+						device.getProductId(),
+						device
+				);
+				mCamera = new SonyCamera(connection, this);
+			} else {
+				//其他设备
+				PtpUsbConnection connection = new PtpUsbConnection(
+						usbManager.openDevice(device),
+						in,
+						out,
+						device.getVendorId(),
+						device.getProductId(),
+						device
+				);
+				mCamera = new PtpCamera(connection, this) {
+					@Override
+					protected void onOperationCodesReceived(Set<Integer> operations) {
+					}
 
-        this.mListener.onNoCameraFound();
-        return false;
-    }
+					@Override
+					protected void queueEventCheck() {
+					}
+				};
+				Log.e(TAG, "不支持的设备id" + device.getVendorId());
+			}
+			return true;
+		}
 
-    @Override
-    public void setPtpListener(PtpListener listener) {
-        this.mListener = listener;
-    }
+		this.mListener.onNoCameraFound();
+		return false;
+	}
 
-    @Override
-    public void onCameraStarted(Camera camera) {
-        this.mListener.onCameraStarted(camera);
-        // 读取存储卡上的照片
-        camera.retrieveStorages(this);
-    }
+	@Override
+	public void setPtpListener(PtpListener listener) {
+		this.mListener = listener;
+	}
 
-    @Override
-    public void onCameraStopped(Camera camera) {
-        this.mListener.onCameraStopped(camera);
-    }
+	@Override
+	public void onCameraStarted(Camera camera) {
+		this.mListener.onCameraStarted(camera);
+		// 读取存储卡上的照片
+		camera.retrieveStorages(this);
+	}
 
-    @Override
-    public void onNoCameraFound() {
-        this.mListener.onNoCameraFound();
-    }
+	@Override
+	public void onCameraStopped(Camera camera) {
+		this.mListener.onCameraStopped(camera);
+	}
 
-    @Override
-    public void onError(String message) {
-        Log.e(TAG, "onError: " + message);
-        this.mListener.onError(message);
-    }
+	@Override
+	public void onNoCameraFound() {
+		this.mListener.onNoCameraFound();
+	}
 
-    @Override
-    public void onCapturedPictureReceived(int objectHandle,
-                                          ObjectInfo objectInfo,
-                                          final Bitmap bitmap) {
-        String path = "";
-        // 组装文件名,然后剔重
-        final String repFilename = objectInfo.filename.substring(0, objectInfo.filename.lastIndexOf('.')) + ".JPG";
+	@Override
+	public void onError(String message) {
+		Log.e(TAG, "onError: " + message);
+		this.mListener.onError(message);
+	}
 
-        new Handler().post(new Runnable() {
-            @Override
-            public void run() {
-                ToastUtil.showShortToast(PtpUsbService.this.mContext, "读取到:" + repFilename);
-            }
-        });
-        try {
-            //7O6A8489.JPG
-            final File files = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Canon/");
-            if (!files.exists()) {
-                files.mkdirs();
-            }
-            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Canon/" + repFilename);
-            FileOutputStream out = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.flush();
-            out.close();
+	@Override
+	public void onCapturedPictureReceived(int objectHandle,
+										  ObjectInfo objectInfo,
+										  final Bitmap bitmap) {
+		String path = "";
+		// 组装文件名,然后剔重
+		final String repFilename = objectInfo.filename.substring(0, objectInfo.filename.lastIndexOf('.')) + ".JPG";
 
-            new Handler().post(new Runnable() {
-                @Override
-                public void run() {
-                    ToastUtil.showShortToast(PtpUsbService.this.mContext, "保存成功:" + files.getAbsolutePath());
-                }
-            });
+		//添加处理，否则报错
+		if (!looperStarted) {
+			Looper.prepare();
+			looperStarted = true;
+		}
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            bitmap.recycle();
-            Log.e(TAG, "读取异常:" + e.toString());
-        }
-    }
+		new Handler().post(new Runnable() {
+			@Override
+			public void run() {
+				ToastUtil.showShortToast(PtpUsbService.this.mContext, "读取到:" + repFilename);
+			}
+		});
+		try {
+			//7O6A8489.JPG
+			final File files = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Canon/");
+			if (!files.exists()) {
+				files.mkdirs();
+			}
+			File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Canon/" + repFilename);
+			FileOutputStream out = new FileOutputStream(file);
+			bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+			out.flush();
+			out.close();
 
-    @Override
-    public void onObjectAdded(int handle) {
-        // 处理文件
-        mCamera.retrievePicture(handle);
-    }
+			new Handler().post(new Runnable() {
+				@Override
+				public void run() {
+					ToastUtil.showShortToast(PtpUsbService.this.mContext, "保存成功:" + files.getAbsolutePath());
+				}
+			});
 
-    @Override
-    public void onPropertyChanged(int p0, int p1) {
+		} catch (Exception e) {
+			e.printStackTrace();
+			bitmap.recycle();
+			Log.e(TAG, "读取异常:" + e.toString());
+		}
+	}
 
-    }
+	@Override
+	public void onObjectAdded(int handle) {
+		// 处理文件
+		mCamera.retrievePicture(handle);
+	}
 
-    /**
-     * 传指定内存卡ID读照片
-     *
-     * @param storageId
-     */
-    @Override
-    public void onAllStorageFound(Integer storageId) {
-        //读照片
-        mCamera.retrieveImageHandles(
-                PtpUsbService.this,
-                storageId,
-                PtpConstants.ObjectFormat.EXIF_JPEG
-        );
-    }
+	@Override
+	public void onPropertyChanged(int p0, int p1) {
 
-    @Override
-    public void onImageHandlesRetrieved(int[] handles) {
-        for (int i = 0; i < handles.length; ++i) {
-            mCamera.retrievePicture(handles[i]);
-        }
-    }
+	}
+
+	/**
+	 * 传指定内存卡ID读照片
+	 *
+	 * @param storageId
+	 */
+	@Override
+	public void onAllStorageFound(Integer storageId) {
+		//读照片
+		mCamera.retrieveImageHandles(
+				PtpUsbService.this,
+				storageId,
+				PtpConstants.ObjectFormat.EXIF_JPEG
+		);
+	}
+
+	@Override
+	public void onImageHandlesRetrieved(int[] handles) {
+		for (int i = 0; i < handles.length; ++i) {
+			mCamera.retrievePicture(handles[i]);
+		}
+	}
 }
